873c924de606c8af7d01184e3ca16487dacbcc79
Rishabh Kumar Gupta <60769940+Rishabh-Kumar-Gupta@users.noreply.github.com>
2020-04-16 03:47:28 +0530

    Update MainClassProducer.java

--- src/main/java/com/rkg/twitter/MainClassProducer.java
+++ src/main/java/com/rkg/twitter/MainClassProducer.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public class MainClassProducer {
     public static void main(String[] args) throws IOException, ParseException {
-        String consumerKey = "IW7FxxxBbC5iULPsC8vqtZUCR";
-        String consumerSecret = "w7pJamZhSlFy78R9PGz9jQu1D9aurPAnala83JfavHD46De9lN";
-        String token = "1247163432345800710-91ONcQJvFWdWK8UqyW1aqzHJdupFur";
-        String secret = "U9wEgdnijN5fr84YXzur19oYWnEsz6d1olUB3k2niGJ3P";
-        String bootstrapServers = "127.0.0.1:9092";
+        String consumerKey = "";
+        String consumerSecret = "";
+        String token = "";
+        String secret = "";
+        String bootstrapServers = "";
         String topic = "twitter_tweet";
         List<String> keyword =  Lists.newArrayList("corona","Corona","Covid","Virus");
 
